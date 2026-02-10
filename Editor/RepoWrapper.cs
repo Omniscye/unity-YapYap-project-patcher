7feab587f8391debc3bace8cc29099f7f4720cc3
Omniscye <103788801+Omniscye@users.noreply.github.com>
2026-02-10 14:16:13 -0500

    Update RepoWrapper.cs

--- Editor/RepoWrapper.cs
+++ Editor/RepoWrapper.cs
@@ -1,17 +1,17 @@
-﻿using Nomnom.UnityProjectPatcher.Editor;
+using Nomnom.UnityProjectPatcher.Editor;
 using Nomnom.UnityProjectPatcher.Editor.Steps;
 
-namespace Kesomannen.RepoProjectPatcher.Editor {
-    [UPPatcher("com.kesomannen.unity-repo-project-patcher")]
-    public static class RepoWrapper {
+namespace Kesomannen.YapYapProjectPatcher.Editor {
+    [UPPatcher("com.kesomannen.unity-yapyap-project-patcher")]
+    public static class YapYapWrapper {
         public static void GetSteps(StepPipeline stepPipeline) {
             stepPipeline.SetInputSystem(InputSystemType.Both);
             stepPipeline.SetGameViewResolution("16:9");
-            stepPipeline.OpenSceneAtEnd("Main");
+            
+            stepPipeline.OpenSceneAtEnd("Bootstrap");
             
             stepPipeline.InsertLast(new FixES3Step());
-            stepPipeline.InsertLast(new GeneratePhotonAssembliesStep());
-            stepPipeline.InsertLast(new ReplaceShadersStep());
+            
         }
     }
 }
