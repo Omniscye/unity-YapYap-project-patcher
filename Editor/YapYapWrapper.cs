5e9ca94eabf1396040d92d7dbdcea2a0fa8aec79
Omniscye <103788801+Omniscye@users.noreply.github.com>
2026-02-10 14:30:59 -0500

    Add shader replacement step to YapYapWrapper

--- Editor/YapYapWrapper.cs
+++ Editor/YapYapWrapper.cs
@@ -2,9 +2,11 @@ using Nomnom.UnityProjectPatcher.Editor;
 using Nomnom.UnityProjectPatcher.Editor.Steps;
 
 namespace Kesomannen.YapYapProjectPatcher.Editor {
+   
     [UPPatcher("com.kesomannen.unity-yapyap-project-patcher")]
     public static class YapYapWrapper {
         public static void GetSteps(StepPipeline stepPipeline) {
+            
             stepPipeline.SetInputSystem(InputSystemType.Both);
             stepPipeline.SetGameViewResolution("16:9");
             
@@ -12,6 +14,7 @@ namespace Kesomannen.YapYapProjectPatcher.Editor {
             
             stepPipeline.InsertLast(new FixES3Step());
             
+            stepPipeline.InsertLast(new ReplaceShadersStep());
         }
     }
 }
